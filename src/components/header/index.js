f2864b2787f261e59e6a133e5b4b4c10ec52998c
SpencerTower <spencerjtower1@gmail.com>
2022-02-14 22:00:33 -0800

    adds scss styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header>
-//         <h1>RESTy</h1>
-//       </header>
-//     );
-//   }
-// }
+import './header.scss';
+
 
 function Header() {
   return (
