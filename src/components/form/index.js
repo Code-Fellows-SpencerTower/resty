f6f0b491d19495ef29d75ec338d01daa1dac1ef3
SpencerTower <spencerjtower1@gmail.com>
2022-02-14 20:07:56 -0800

    converts Form class component to function component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,36 +2,38 @@ import React from 'react';
 
 import './form.scss';
 
-class Form extends React.Component {
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    this.props.handleApiCall(formData);
-  }
+function handleSubmit(e, props) {
+  e.preventDefault();
+  const formData = {
+    method: 'GET',
+    url: 'https://pokeapi.co/api/v2/pokemon',
+  };
+  props.handleApiCall(formData);
+}
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
+function Form(props) {
+
+  handleSubmit(props);
+
+  return (
+    <>
+      <form onSubmit={this.handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input name='url' type='text' />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span id="get">GET</span>
+          <span id="post">POST</span>
+          <span id="put">PUT</span>
+          <span id="delete">DELETE</span>
+        </label>
+      </form>
+    </>
+  );
+
 }
 
 export default Form;
